db5819d3796ba6d4d9a6f8845c45c4cd9fb795ba
Colin Ruiz <CRuiz@clarku.edu>
2024-02-28 18:32:16 -0500

    updating the user model

--- cs244webdev/textswap/models.py
+++ cs244webdev/textswap/models.py
@@ -4,3 +4,5 @@ class User(models.Model):
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
+    is_landlord = models.BooleanField(default=False)  # Boolean field for landlord status
+    is_student = models.BooleanField(default=False)
