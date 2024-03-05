a84ecc0cd711d2c30aa12ff6cb6a3a821ed1e6a9
Colin Ruiz <CRuiz@clarku.edu>
2024-03-05 13:52:20 -0500

    finishing touches on regristration html. added a style css for the navbar

--- cs244webdev/cs244webdev/settings.py
+++ cs244webdev/cs244webdev/settings.py
@@ -118,6 +118,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Only for production
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
