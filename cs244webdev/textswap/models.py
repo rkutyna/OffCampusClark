438162be457befd0d150a41bacca61df32b60917
rkutyna <67471832+rkutyna@users.noreply.github.com>
2024-04-03 12:10:06 -0400

    Changed Structure of models

--- cs244webdev/textswap/models.py
+++ cs244webdev/textswap/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User as Auth_user
+from django.db.models.functions import Now
 
 class User(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+    parent_user = models.ForeignKey(Auth_user, on_delete=models.CASCADE, null=True)
     is_landlord = models.BooleanField(default=False)  # Boolean field for landlord status
     is_student = models.BooleanField(default=False)
         
@@ -17,8 +17,14 @@ class Apartment(models.Model):
     rent = models.IntegerField()
     move_in_date = models.DateField()
     lease_length = models.CharField(max_length = 50)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Auth_user, on_delete=models.CASCADE)
     pets_allowed = models.BooleanField()
     num_beds = models.IntegerField()
     num_baths = models.IntegerField()
     num_sqr_ft = models.IntegerField()
+    
+class Message(models.Model):
+    user1_id = models.ForeignKey(Auth_user, on_delete=models.CASCADE, related_name='sender')
+    user2_id = models.ForeignKey(Auth_user, on_delete=models.CASCADE, related_name='receiver')
+    time_sent = models.DateTimeField(db_default=Now())
+    content = models.TextField()
