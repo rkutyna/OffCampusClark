bcbcecd6f9bdee665ffd4c7300aa217aa2849528
rkutyna <67471832+rkutyna@users.noreply.github.com>
2024-04-05 17:50:53 -0400

    fixed images

--- cs244webdev/textswap/models.py
+++ cs244webdev/textswap/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User as Auth_user
 from django.db.models.functions import Now
 from datetime import datetime
+import os
 
 
 
@@ -9,10 +10,15 @@ class User(models.Model):
     parent_user = models.ForeignKey(Auth_user, on_delete=models.CASCADE, null=True)
     is_landlord = models.BooleanField(default=False)  # Boolean field for landlord status
     is_student = models.BooleanField(default=False)
+    
+def get_upload_path(instance, filename):
+    return os.path.join('textswap/static/images', filename)
         
 class Apartment(models.Model):
     address = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to='static/images')
+    #photo = models.ImageField(upload_to='textswap/static/images')
+    photo_name = models.CharField(max_length=100, default=None)  # New field to store the photo name
+    photo = models.ImageField(upload_to=get_upload_path)  # Use a custom function for upload_to
     description = models.TextField()
     dishwasher = models.BooleanField()
     washer_dryer = models.BooleanField(default = False)
@@ -25,6 +31,13 @@ class Apartment(models.Model):
     num_beds = models.IntegerField()
     num_baths = models.IntegerField()
     num_sqr_ft = models.IntegerField()
+    
+    def save(self, *args, **kwargs):
+        if not self.photo_name:
+            original_filename = os.path.basename(self.photo.name)
+            self.photo_name = original_filename.replace(' ', '_')
+        print(self.photo_name)
+        super().save(*args, **kwargs)
     
 class Message(models.Model):
     user1_id = models.ForeignKey(Auth_user, on_delete=models.CASCADE, related_name='sender')
