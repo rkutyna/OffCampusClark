532f0dc334a62259ccb9d454b63f9d7076228959
rkutyna <67471832+rkutyna@users.noreply.github.com>
2024-06-05 14:04:31 -0400

    Fixed User Listings displaying the wrong page

--- django/djangoproject/textswap/urls.py
+++ django/djangoproject/textswap/urls.py
@@ -11,9 +11,12 @@ urlpatterns = [
     path('filtered_apartments/', filtered_apartments, name='filtered_apartments'),
     path('create/', create_apartment_view, name='create_apartment'),
     path('apartment/<int:pk>/', apartment_detail_view, name='apartment_detail'),
-    path('user/listings/', user_listings_view, name='user_listings'),
+    path('user_listings/', user_listings_view, name='user_listings'),
     path('edit_apartment/<int:apartment_id>/', edit_apartment, name='edit_apartment'),
     path('logout/', logout_view, name='logout'),
-    path('delete_apartment/<int:apartment_id>/', delete_apartment, name='delete_apartment')
+    path('delete_apartment/<int:apartment_id>/', delete_apartment, name='delete_apartment'),
     # Add other URLs as needed
+    
+    path('create-message/', create_message, name='create-message'),
+    path('stream-chat-messages/', stream_chat_messages, name='stream-chat-messages'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
