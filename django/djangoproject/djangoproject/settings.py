376a40612c1a533bba612ee170d31bd727e55766
rkutyna <67471832+rkutyna@users.noreply.github.com>
2025-06-26 16:07:50 -0400

    refactor: simplify host configuration by replacing DJANGO_PUBLIC_HOSTNAME with comma-separated ALLOWED_HOSTS_FROM_ENV

--- django/djangoproject/djangoproject/settings.py
+++ django/djangoproject/djangoproject/settings.py
@@ -23,15 +23,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ["DJANGO_DEBUG"] == "True" else False
+DEBUG = True if os.environ.get("DJANGO_DEBUG") == "True" else False
 
+# These hosts are required for the application to run correctly in Docker
 ALLOWED_HOSTS = [
-    os.environ["DJANGO_PUBLIC_HOSTNAME"],
     "django",
     "nginx",
-    "127.0.0.1",
-    os.environ["CURRENT_HOST"],
 ]
+
+# Add additional hosts from a comma-separated string in the .env file
+# For example: ALLOWED_HOSTS_FROM_ENV=127.0.0.1,localhost,mydomain.com
+additional_hosts = os.environ.get('ALLOWED_HOSTS_FROM_ENV', '').split(',')
+ALLOWED_HOSTS.extend([host.strip() for host in additional_hosts if host.strip()])
 
 # Application definition
 INSTALLED_APPS = [
